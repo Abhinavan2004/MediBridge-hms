30804a6388233e4a0a8198fa5a01efdc05b53de9
Abhinavan2004 <abhinavanpan@gmail.com>
2025-08-28 16:34:33 +0530

    Added Appointment reltd files

--- Database Codes/hospital_ms db codes.sql
+++ Database Codes/hospital_ms db codes.sql
@@ -11,6 +11,7 @@ create table user_account(id int AUTO_INCREMENT PRIMARY KEY,
 ALTER TABLE user_account 
 MODIFY id INT AUTO_INCREMENT PRIMARY KEY;
 
+/* DOCTOR TABLE */
 create table Doctor_record(id int auto_increment primary key ,
                                doc_name varchar(30),
                                doc_dob varchar(15),
@@ -21,11 +22,27 @@ create table Doctor_record(id int auto_increment primary key ,
                                );
                                
 ALTER table Doctor_record ADD COLUMN doc_password varchar(20) ;
-                         
+          
+/* SPECIALIST TABLE */
 create table specialist_record(id int auto_increment primary key ,
                                spec_name varchar(30),
                                category varchar(30),
                                contact_no varchar(10));
+                               
+                               
+
+/* APPOINTMENT TABLE */
+-- create table appointment_record(user_id int NOT NULL,
+--                                 app_name varchar(30),
+--                                 app_gender varchar(10),
+--                                 app_age int ,
+--                                 app_date date ,
+--                                 app_cond varchar(40),
+--                                 app_contact varchar(10),
+--                                 app_email varchar(30),
+--                                 
+                                
+
 Select * from specialist_record ;
 
 Select * from Doctor_record ;
